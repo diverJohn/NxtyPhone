f0084983785f2c96b4ad3044fc62a74d54c5f450
diverJohn <divers@ix.netcom.com>
2014-07-28 12:50:05 -0700

    a

--- www/js/tech.js
+++ www/js/tech.js
@@ -14,10 +14,9 @@ var tech = {
 	{
 		var myHtml = 
 			"<img src='img/header_tech.png' width='100%' />" +
+			"<button type='button' class='back_icon' onclick='tech.handleBackKey()'><img src='img/go_back.png'/></button>"+
 			"<div class='bt_icon'><img src='img/bluetooth_off.png' /></div>" +
-            "<div class='reg_icon'><img src='img/reg_no.png' /></div>" +
-            "<button type='button' class='back_icon' onclick='tech.handleBackKey()'><img src='img/go_back.png'/></button>"
-
+            "<div class='reg_icon'><img src='img/reg_no.png' /></div>"
 
 		$('body').html(myHtml);  			
 	},
