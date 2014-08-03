361c2b66be8d9a587ba86c46ee973f6d5be06961
diverJohn <divers@ix.netcom.com>
2014-08-03 16:23:31 -0700

    Last from home - procRx does not work

--- www/js/msg_nxty.js
+++ www/js/msg_nxty.js
@@ -91,70 +91,6 @@ unsigned char ProcessNxtyRxMsg( unsigned char * pRxMsgData, unsigned char uLen )
 
 
 
-void SendNxtyMsg( unsigned char uCmd, unsigned char * pMsgData, unsigned char uLen )
-{
-  unsigned char   i;
-  unsigned char   uCrc;
-  unsigned char   uStdBuff[NXTY_STD_MSG_SIZE];
-  unsigned char   uBigBuff[NXTY_BIG_MSG_SIZE];
-
-
-  if( uLen > (NXTY_BIG_MSG_SIZE-3) )
-  {
-    // Msg len too big...
-    return;
-  }
-      
-
-  
-  // Check for STD message size...
-  if( (uLen + 3) <= NXTY_STD_MSG_SIZE )
-  {
-    memset( uStdBuff, NXTY_MSG_FILL_BYTE, sizeof(uStdBuff) );
-    uStdBuff[0] = NXTY_STD_MSG_SIZE;
-    uStdBuff[1] = uCmd;
-  
-    if( uLen && pMsgData )
-    {
-      for( i = 0; i < uLen; i++ )
-      {
-        uStdBuff[2+i] = *pMsgData++;
-      }
-    }
-
-    // Calculate the CRC...
-    uCrc     = 0;
-    CalcCrc8( uStdBuff, NXTY_STD_MSG_SIZE-1, &uCrc );
-    uStdBuff[NXTY_STD_MSG_SIZE-1] = uCrc;
-
-    // Send the data..
-    PrintBuffer( "Tx: ", (unsigned char *)uStdBuff, NXTY_STD_MSG_SIZE );
-    Transmit( uStdBuff, NXTY_STD_MSG_SIZE );
-  }
-  else
-  {
-
-    memset( uBigBuff, NXTY_MSG_FILL_BYTE, sizeof(uBigBuff) );
-    uBigBuff[0] = NXTY_BIG_MSG_SIZE;   
-    uBigBuff[1] = uCmd;
-
-    if( uLen && pMsgData )
-    {
-      for( i = 0; i < uLen; i++ )
-      {
-        uBigBuff[2+i] = *pMsgData++;
-      }
-    }
-
-    // Calculate the CRC...
-    uCrc = 0;
-    CalcCrc8( uBigBuff, NXTY_BIG_MSG_SIZE-1, &uCrc );
-    uBigBuff[NXTY_BIG_MSG_SIZE-1] = uCrc;
-
-    // Send the data..
-    PrintBuffer( "Tx: ", (unsigned char *)uBigBuff, NXTY_BIG_MSG_SIZE );
-    Transmit( uBigBuff, NXTY_BIG_MSG_SIZE );
-  }
   
 
 
@@ -163,42 +99,6 @@ void SendNxtyMsg( unsigned char uCmd, unsigned char * pMsgData, unsigned char uL
 
 
 
-#define NXTY_STD_MSG_SIZE                   (12)
-#define NXTY_BIG_MSG_SIZE                   (255)
-#define NXTY_MSG_FILL_BYTE                  (0x00)   
-
-
-#define NXTY_SYS_SN_REQ                     (0x01)
-#define NXTY_SYS_SN_RSP                     (0x41)
-#define NXTY_SET_BLUETOOTH_CNX_STATUS_REQ   (0x02)
-#define   BLUETOOTH_NOT_CNX                   (0x00)
-#define   BLUETOOTH_CNX                       (0x01)
-#define NXTY_SET_BLUETOOTH_CNX_STATUS_RSP   (0x42)
-#define NXTY_CELL_INFO_REQ                  (0x03)
-#define NXTY_CELL_INFO_RSP                  (0x43)
-#define NXTY_REGISTRATION_REQ               (0x04)
-#define NXTY_REGISTRATION_RSP               (0x44)
-#define NXTY_GET_MON_MODE_HEADINGS_REQ      (0x05)
-#define NXTY_GET_MON_MODE_HEADINGS_RSP      (0x45)
-#define NXTY_GET_MON_MODE_PAGE_REQ          (0x06)
-#define NXTY_GET_MON_MODE_PAGE_RSP          (0x46)
-#define NXTY_SW_VERSION_REQ                 (0x07)
-#define NXTY_SW_VERSION_RSP                 (0x47)
-#define NXTY_DOWNLOAD_START_REQ             (0x08)
-#define NXTY_DOWNLOAD_START_RSP             (0x48)
-#define NXTY_DOWNLOAD_TRANSFER_REQ          (0x09)
-#define NXTY_DOWNLOAD_TRANSFER_RSP          (0x49)
-#define NXTY_DOWNLOAD_END_REQ               (0x0A)
-#define NXTY_DOWNLOAD_END_RSP               (0x4A)
-
-#define NXTY_STATUS_RSP                     (0x4B)
-
-   
-// Errors processing...
-#define NXTY_INVALID_BUFFER_ERR             (0xF0)
-#define NXTY_INVALID_LEN_ERR                (0xF1)
-#define NXTY_INVALID_CRC_ERR                (0xF2)
-#define NXTY_INVALID_COMMAND_ERR            (0xF3)   
 
 
 */
@@ -329,15 +229,83 @@ var nxty = {
         }
         console.log( "Tx: " + outText );
         
-        var x = uBigBuff.subarray(0,100);  // 40 works, 60 works, 80 ok
+        var x = uBigBuff.subarray(0,80);  // 40 works, 60 works, 80 ok, 100 no good...
         
         WriteBluetoothDevice(x);
       }
-      
-    
-    
     
     },
+     
+     
+     
+    ProcessNextyRxMsg: function( pRxMsgData, uLenByte )
+    {
+        var i;
+        var uCrc     = new Uint8Array(1);
+        var uCmd     = new Uint8Array(1);
+        
+    
+//      unsigned char   uRtn;
+//      unsigned char   uCrc;
+//      unsigned char   uCmd;
+    
+      
+    
+      // Error processing...
+      if( !((pRxMsgData[0] == NXTY_STD_MSG_SIZE) || (pRxMsgData[0] == NXTY_BIG_MSG_SIZE)) )
+      {
+        printf( "**** Err: Message len, 1st byte should be 12 or 255, len = %d\r\n", pRxMsgData[0] );
+        return( NXTY_INVALID_LEN_ERR );
+      }
+      else if( pRxMsgData == NULL )
+      {
+        printf( "**** Err: Null pointer buffer\r\n" );
+        return( NXTY_INVALID_BUFFER_ERR );
+      }
+      else if( uLen == 0 )
+      {
+        printf( "**** Err: 0 len\r\n" );
+        return( NXTY_INVALID_LEN_ERR );
+      }
+      
+      uCrc = 0;
+      CalcCrc8( pRxMsgData, pRxMsgData[0]-1, &uCrc );
+      
+      if( pRxMsgData[pRxMsgData[0]-1] != uCrc )
+      {
+        printf( "**** Err: Invalid CRC: expected: 0x%02X  calc: 0x%02X\r\n", pRxMsgData[pRxMsgData[0]-1], uCrc );
+        return( NXTY_INVALID_LEN_ERR );
+      }
+    
+      uCmd = ((S_NXTY_HEADING *)pRxMsgData)->uCmd;
+      uRtn = uCmd;
+    
+      switch( uCmd )
+      {
+        case NXTY_SYS_SN_RSP:                     printf( "System SN Rsp" );                break;
+        case NXTY_SET_BLUETOOTH_CNX_STATUS_RSP:   printf( "Set Bluetooth Cnx Status Rsp" ); break;
+        case NXTY_CELL_INFO_RSP:                  printf( "Cell Info Rsp" );                break;
+        case NXTY_REGISTRATION_RSP:               printf( "Registration Rsp" );             break;
+        case NXTY_GET_MON_MODE_HEADINGS_RSP:      printf( "Get Mon Mode Headings Rsp" );    break;
+        case NXTY_GET_MON_MODE_PAGE_RSP:          printf( "Get Mon Mode Page Rsp" );        break;
+        case NXTY_SW_VERSION_RSP:                 printf( "SW Version Rsp" );               break;
+        case NXTY_DOWNLOAD_START_RSP:             printf( "Download Start Rsp" );           break;
+        case NXTY_DOWNLOAD_TRANSFER_RSP:          printf( "Download Transfer Rsp" );        break;
+        case NXTY_DOWNLOAD_END_RSP:               printf( "Download End Rsp" );             break;
+        case NXTY_STATUS_RSP:                     printf( "Status Rsp" );                   break;
+    
+        
+        default:
+        {
+          uRtn = NXTY_INVALID_COMMAND_ERR;
+          break;
+        }
+      }
+      
+    },
+     
+     
+     
      
 
     CalcCrc8: function( dataBytes, uLen, crcByte )
