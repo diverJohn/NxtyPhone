8ebef589e5426260ef6daea3863bada5c61a51ea
diverJohn <divers@ix.netcom.com>
2014-08-02 17:27:15 -0700

    1.0.2

--- www/js/bluetoothle.js
+++ www/js/bluetoothle.js
@@ -475,10 +475,10 @@ function WriteBluetoothDevice( u8 )
     var u64 = bluetoothle.getString(u8);
 
     // Version 1.0.0 of the plugin
-    var paramsObj = {"value":u64, "serviceAssignedNumber":bridgeServiceUuid, "characteristicAssignedNumber":bridgeRxCharacteristicUuid};
+//    var paramsObj = {"value":u64, "serviceAssignedNumber":bridgeServiceUuid, "characteristicAssignedNumber":bridgeRxCharacteristicUuid};
 
     // 1.0.2 of the plugin 
-//    var paramsObj = {"value":u64, "serviceUuid":bridgeServiceUuid, "characteristicUuid":bridgeRxCharacteristicUuid};
+    var paramsObj = {"value":u64, "serviceUuid":bridgeServiceUuid, "characteristicUuid":bridgeRxCharacteristicUuid};
     
     bluetoothle.write(writeSuccess, writeError, paramsObj);
 }
@@ -492,7 +492,7 @@ function writeSuccess(obj)
         console.log("BT: Write data sent successfully");
 
         // Must re-subscribe after every write with version 1.0.0 of the plugin...
-        SubscribeBluetoothDevice();
+//        SubscribeBluetoothDevice();
     }
     else
     {
