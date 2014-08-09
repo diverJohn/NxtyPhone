17518f383685fa84fc16e1e5b8a2c293ed99dd7c
diverJohn <divers@ix.netcom.com>
2014-08-08 17:44:48 -0700

    a

--- www/js/tech.js
+++ www/js/tech.js
@@ -12,7 +12,7 @@ var tech = {
 
 	renderTechView: function() 
 	{
-/*
+
 		var myBluetoothIcon = isBluetoothCnx ? "<div id='bt_icon_id' class='bt_icon'>" + szBtIconOn + "</div>" : "<div  id='bt_icon_id' class='bt_icon'>" + szBtIconOff + "</div>";
 		var myRegIcon       = isRegistered   ? "<div id='reg_icon_id' class='reg_icon'>" + szRegIconOn + "</div>" : "<div id='reg_icon_id' class='reg_icon'>" + szRegIconOff + "</div>";
 		
@@ -20,40 +20,27 @@ var tech = {
 			"<img src='img/header_tech.png' width='100%' />" +
 			"<button type='button' class='back_icon' onclick='tech.handleBackKey()'><img src='img/go_back.png'/></button>"+
 			myRegIcon +
-            myBluetoothIcon
-*/
+            myBluetoothIcon +
+            
+            
+ "<h1>UNII Engineering Data</h1>" +
+ "<table align='center'>" +
+"<tr> <th>Description</th>  <th>Value</th></tr>" +
+"<tr> <td id='v1'>5 GHz DL Freq</td>  <td id='v1'>0</td></tr>" +
+"<tr>  <td id='v1'>5 GHz UL Freq</td>  <td id='v2'>0</td></tr>" +
+"<tr>  <td id='v1'>UNII Modem State</td>  <td id='v3'>Down</td></tr>" +
+"<tr>  <td id='v1'>NU RSSI</td>  <td id='v4'>0</td></tr>" +
+"<tr>  <td id='v1'>CU RSSI</td>  <td id='v5'>0</td></tr>" +
+"<tr>  <td id='v1'>NU Tx Pwr</td>  <td id='v6'>0</td></tr>" +
+"<tr>  <td id='v1'>CU Tx Pwr</td>  <td id='v7'>0</td></tr>" +
+"<tr>  <td id='v1'>Ctrl Chan BER</td>  <td id='v8'>0</td></tr>" +
+"<tr>  <td id='v1'>Radar Detect Cnt</td>  <td id='v9'>0</td></tr>" +
+"<tr>  <td id='v1'>Distance Metric</td>  <td id='v10'>0</td></tr>" +
+"<tr>  <td id='v1'>ID</td>  <td id='v11'>0</td></tr>" +
+"</table>";
+            
+            
 
-
-var myStyle = 
-"<style>" +
-"* { padding: 0; margin: 0 }" +
-"body { height: 100%; white-space: nowrap }" +
-"html { height: 100% }" +
-".red { background: red }" +
-".blue { background: blue }" +
-".yellow { background: yellow }" +
-".header { width: 100%; height: 10%; position: fixed }" +
-".wrapper { width: 1000%; height: 100%; background: green }" +
-".page { width: 10%; height: 100%; float: left; -webkit-overflow-scrolling: touch; }" +
-"</style>";
-
-
-var myHtml = 
-"<div class='header red'></div>" +
-"<div class='wrapper'>" +
-    "<div class='page yellow'></div>" +
-    "<div class='page blue'></div>"   +
-    "<div class='page yellow'></div>" +
-    "<div class='page blue'></div>"   +
-            "<div class='page yellow'></div>" +
-    "<div class='page blue'></div>"   +
-        "<div class='page yellow'></div>" +
-    "<div class='page blue'></div>"   +
-        "<div class='page yellow'></div>" +
-    "<div class='page blue'></div>" +
-"</div>";
-
-        $('head').html(myStyle); 
 		$('body').html(myHtml);  			
 	},
 
