ff394bf64df930c34843ed569d442a4c73186df9
diverJohn <divers@ix.netcom.com>
2014-08-15 10:58:43 -0700

    a

--- www/js/main.js
+++ www/js/main.js
@@ -52,7 +52,7 @@ function HandleButtonDown()
 function HandleButtonUp()
 {
 	$(this).css("opacity","0.5");
-//	$(this).css("outline", "none" );
+	$(this).css("outline", "none" );       // Used to remove orange box for android 4+
 }
 
 // SendCloudData............................................................................................
