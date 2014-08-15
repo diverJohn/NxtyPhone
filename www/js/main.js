4b85e1a36e7c2d1b0596d236d74a8c5a76f25493
diverJohn <divers@ix.netcom.com>
2014-08-15 11:50:43 -0700

    a

--- www/js/main.js
+++ www/js/main.js
@@ -186,7 +186,7 @@ reg.renderRegView();
 			"<img src='img/header_main.png' width='100%' />" +
 			myBluetoothIcon +
             myRegIcon +
-            "<div class='background'><img src='img/background.png' width='100%'/>" +
+            "<div class='background'><img src='img/background.png'/>" +
    			"<button id='sw_button_id' type='button' class='mybutton' onclick='app.handleSwUpdateKey()'><img src='img/button_SwUpdate.png' /></button>" +
 			"<button id='tk_button_id' type='button' class='mybutton' onclick='app.handleTechModeKey()'><img src='img/button_TechMode.png'/></button>" +
   			myRegButton +
