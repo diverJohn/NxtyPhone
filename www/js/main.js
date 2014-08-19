cd815aa6702fb57c47a936a8bce2213a4ac69f59
diverJohn <divers@ix.netcom.com>
2014-08-18 20:42:46 -0700

    a

--- www/js/main.js
+++ www/js/main.js
@@ -223,11 +223,11 @@ reg.renderRegView();
 			"<img src='img/header_main.png' width='100%' />" +
    			myBluetoothIcon +
             myRegIcon +
+            "<br><br><br><br><br><br><br><br><br><br>" +
    			"<button id='sw_button_id' type='button' class='mybutton' onclick='app.handleSwUpdateKey()'><img src='img/button_SwUpdate.png' /></button>" +
 			"<button id='tk_button_id' type='button' class='mybutton' onclick='app.handleTechModeKey()'><img src='img/button_TechMode.png'/></button>" +
   			myRegButton +
-  			szMyStatusLine
-  			"<br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+  			szMyStatusLine;
   			
 		$('body').html(myHtml); 
 		
