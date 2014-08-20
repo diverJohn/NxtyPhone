4a594bf48cfae33e68aed56adef8bc64098772b0
diverJohn <divers@ix.netcom.com>
2014-08-20 08:43:58 -0700

    a

--- www/js/msg_nxty.js
+++ www/js/msg_nxty.js
@@ -107,6 +107,7 @@ var nxty = {
         return;
       }        
         
+/*        
       if( msgRxLastCmd == NXTY_WAITING_FOR_RSP )
       {
         uTxMsgNotReadyCnt++;
@@ -121,6 +122,7 @@ var nxty = {
             PrintLog(99,  "Msg: Tx requested before Rx received. TxNotReadyCnt = " + uTxMsgNotReadyCnt + " send Tx and clear count." );
         }
       }
+*/
 
       uTxMsgNotReadyCnt = 0;
       
